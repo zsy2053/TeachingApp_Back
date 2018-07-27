8a8021b0868e4b9e6e27a39798f0ce7cc9ebf645
Shuyang Zang <shuyangzang@Shuyangs-MacBook-Pro.local>
2018-07-26 23:12:39 -0400

    Improved authentication

--- models/UserModel.js
+++ models/UserModel.js
@@ -4,20 +4,50 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema({
   username: {
     type: String,
-    required: 'Please enter the username',
+    required: true,
+    trim: true,
     unique: true
   },
   email: {
     type: String,
-    required: 'Please enter your email address',
+    required: true,
+    trim: true,
     unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  passwordconfirm: {
+    type: String,
+    required: true
   },
   status: {
     type: [{
       type: String,
+      required: true,
       enum: ['teacher', 'student']
     }]
   }
 });
+
+UserSchema.pre('save', (next) => {
+  var user = this
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) {
+      return next(err)
+    }
+    user.password = hash
+    next()
+  })
+
+  bcrypt.hash(user.passwordconfirm, 10, (err, hash) => {
+    if (err) {
+      return next(err)
+    }
+    user.passwordconfirm = hash
+    next()
+  })
+})
 
 module.exports = mongoose.model('User', UserSchema);
